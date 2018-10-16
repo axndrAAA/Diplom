0cc46e4b223baabb946777fdc9d5e32d89e4579a
axndrAAA <axndr-AAA@yandex.ru>
2018-10-17 01:17:47 +0300

    Все сделано, но не протестировано

--- BruteForce/BruteForce.py
+++ BruteForce/BruteForce.py
@@ -2,8 +2,12 @@
 
 import numpy as np
 import BruteForce.InitialConditions as ic
+import BruteForce.Measures as criteria
 
-X_lkq = np.zeros(ic.L,ic.K,ic.Q)
+X_lkq = np.zeros((ic.L,ic.K,ic.Q))
+
+J_max = 0
+X_lkq_optimal = np.zeros((ic.L,ic.K,ic.Q))
 
 for l in range(ic.L):
     for k in range(ic.K):
@@ -12,3 +16,12 @@ for l in range(ic.L):
             for i in range(ic.A_ql[q,l]):
                 X_lkq[l,k,q] = i
                 #вычисление значения критерия
+                J = criteria.J(X_lkq)
+                if (J > J_max):
+                    J = J_max
+                    X_lkq_optimal = X_lkq
+                else:
+                    continue
+print("Значение критерия J = %f. Оптимальная матрица целераспределения:" % J_max)
+
+print(X_lkq_optimal)
--- BruteForce/Measures.py
+++ BruteForce/Measures.py
@@ -1,4 +1,5 @@
 import BruteForce.InitialConditions as ic
+import math as m
 
 
 def J1(x_lkq):
@@ -19,11 +20,28 @@ def J2(x_lkq):
         for l in range(ic.L):
             for q in range(ic.Q):
                 d_sum += ic.p_l[l] * ic.r_lk[l,k]*x_lkq[l,k,q]/ic.p_l.max()
+
     return d_sum/x_lkq.sum()
 
 
 def J3(x_lkq):
-    return 1
+    sum = 0
+    for k in range(ic.K):
+        for l in range(ic.L):
+            for q in range(ic.Q):
+                sum += x_lkq[l,k,q]*getDzitta_qkl(q,k,l)/(ic.T_k[k].t_max - ic.T_k[k].t_min)
+
+    sum /= x_lkq.sum()
+    return sum
 
 def J(x_lkq):
     return ic.alpha[0]*J1(x_lkq) - ic.alpha[1]*J2(x_lkq) + ic.alpha[2]*J3(x_lkq)
+
+def getDzitta_qkl(q,k,l):
+    d_qk = getD_qk(q,k)
+    ret =  0.5*((d_qk/ic.V_l[l].v_min - d_qk/ic.V_l[l].v_max) + (ic.T_k[k].t_max - ic.T_k[k].t_min)
+                - m.fabs(ic.T_k[k].t_min -  d_qk/ic.V_l[l].v_max)-m.fabs(ic.T_k[k].t_max -  d_qk/ic.V_l[l].v_min))
+    return ret
+
+def getD_qk(q,k):
+    return m.sqrt(m.pow((ic.X_q[q].x - ic.X_k[k].x),2) + m.pow((ic.X_q[q].y - ic.X_k[k].y),2))
