41283272cf4f86803fc437c1992cff72f6c4b2eb
axndrAAA <axndr-AAA@yandex.ru>
2018-11-18 03:18:37 +0300

    Запилен макет с сохраненим матриц и их перебором. Функция универсальна. Размерности выходных матриц не совпадают с lkq (индексы перепутаны, потому что нехрен было q ставить полследним) возможно теряется часть перестановок (как и ранее в нерекурсивном алгоритме) считает слишком долго нужно дебажить

--- BruteForce/BruteForce.py
+++ BruteForce/BruteForce.py
@@ -86,7 +86,7 @@ X_lkq = np.zeros((ic.L, ic.K, ic.Q))
 # Формируем возможные комбинации для каждого из аэродромов
 prmts_ql = []
 max_prmts_count = 0
-for q in range(1): # debug ic.Q
+for q in range(ic.Q):
     mq = []
     a = 1
     for l in range(ic.L):
@@ -101,35 +101,46 @@ print('Макс. число перестановок: ' + str(max_prmts_count))
 
 optDef = OptimalDefinition()  # объект содержащий оптимальное решение
 
+# Собираем все возможные варианты матриц для каждого аэродрома
+x_q = []
+for q in range(ic.Q):
+    # все возможные матрицы для данного аэродрома q
+    x_matrixes = Permutation.permute(prmts_ql[q])
+
+    # сохраняем
+    x_q.append(x_matrixes)
+
+# теперь перебираем матрицы аэродромов
+matrixes = Permutation.permute(x_q)
+
 # nl_indexes = np.zeros(ic.L, dtype=np.int)*-1
 # l_boost(0, nl_indexes, prmts_ql, X_lkq, optDef)
 # nlq_indexes = np.zeros([ic.L,ic.Q], dtype=np.int)*(-1)
 # ql_boost(0, 0, nlq_indexes, prmts_ql, X_lkq, optDef)
-
-nl_indexes = np.zeros(ic.L, dtype=np.int)
-l = 0
-while True:
-    if l == ic.L - 1:
-        for nl_indexes[l] in range(len(prmts_ql[0][l])):
-            X_lkq[l, :, 0] = prmts_ql[0][l][nl_indexes[l]]
-            optDef.checkMatrix(X_lkq)
-        if nl_indexes[0] == 13:
-            l = l
-        l -= 1
-    else:
-        if nl_indexes[l] < len(prmts_ql[0][l]):
-            X_lkq[l, :, 0] = prmts_ql[0][l][nl_indexes[l]]
-            nl_indexes[l] += 1
-            l += 1
-
-        else:
-            nl_indexes[l] = 0
-            l -= 1
-
-    # условие выхода
-    if nl_indexes[0] == len(prmts_ql[0][0]):
-        break
-
+# nl_indexes = np.zeros(ic.L, dtype=np.int)
+# l = 0
+# while True:
+#     if l == ic.L - 1:
+#         for nl_indexes[l] in range(len(prmts_ql[0][l])):
+#             X_lkq[l, :, 0] = prmts_ql[0][l][nl_indexes[l]]
+#             optDef.checkMatrix(X_lkq)
+#         if nl_indexes[0] == 13:
+#             l = l
+#         l -= 1
+#     else:
+#         if nl_indexes[l] < len(prmts_ql[0][l]):
+#             X_lkq[l, :, 0] = prmts_ql[0][l][nl_indexes[l]]
+#             nl_indexes[l] += 1
+#             l += 1
+#
+#         else:
+#             nl_indexes[l] = 0
+#             l -= 1
+#
+#     # условие выхода
+#     if nl_indexes[0] == len(prmts_ql[0][0]):
+#         break
+#
 
 
 
--- BruteForce/Permutations.py
+++ BruteForce/Permutations.py
@@ -1,6 +1,7 @@
 #класс будет выдавать все возможные комбинации последовательностей чисел, для которых сумма не будет превышать заданного числа
 import  numpy as np
 import itertools as it
+import InitialConditions as ic
 class Permutation:
     sum_condition = 0
     lst = 0
@@ -16,6 +17,7 @@ class Permutation:
             if(last.sum() <= self.sum_condition):
                 return last
         return None
+
     def getAll(self):
         ret = []
         while True:
@@ -23,12 +25,40 @@ class Permutation:
             if(not arr.any()):
                break
             ret.append(arr);
-            # last = self.lst.pop()
-            # arr = np.zeros(len(last))
-            # for i in range(arr.size):
-            #     arr[i] = int(last[i])
-            # if(arr.sum() <= self.sum_condition):
-            #     ret.append(arr)
         return ret
 
+    @staticmethod
+    def permute(prmts_ln):
+        """
+            permute возвращает все возможные комбинации каждого элемента c каждым эелементом из всех групп L
+            prmts_ln - содержит массивы возможных распределений для каждого типа l
 
+        """
+
+        ret = []
+        x = [0] * len(prmts_ln)
+
+        l = 0
+        nl_indexes = np.zeros(len(prmts_ln), dtype=np.int)
+
+        while True:
+            if l == ic.L - 1:
+                for nl_indexes[l] in range(len(prmts_ln[l])):
+                    x[l] = prmts_ln[l][nl_indexes[l]]
+                    ret.append(np.array(x))
+                l -= 1
+            else:
+                if nl_indexes[l] < len(prmts_ln[l]):
+                    x[l] = prmts_ln[l][nl_indexes[l]]
+                    nl_indexes[l] += 1
+                    l += 1
+
+                else:
+                    nl_indexes[l] = 0
+                    l -= 1
+
+            # условие выхода
+            if nl_indexes[0] == len(prmts_ln[0]):
+                break
+
+        return ret
